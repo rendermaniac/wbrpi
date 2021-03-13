a3cd39a3b072eae94a06f9d3f50fc78206314d3a
Simon Bunker <simon@rendermania.com>
2021-03-13 20:36:41 +1100

    added camera support

--- flight_computer3.py
+++ flight_computer3.py
@@ -12,11 +12,15 @@ import board
 import busio
 import adafruit_bmp280
 
+import picamera
+
 SERVO_PIN = 13
 PARACHUTE_DEPLOY = 1500
 PARACHUTE_RESET = 500
 
 DROP_AFTER_APOGEE = 5.0
+
+TELEMETRY_PATH = "/home/pi/"
 
 from paho.mqtt import client as mqtt_client
 
@@ -25,7 +29,9 @@ def on_message(client, rocket, msg):
     if msg.topic == "/rocket/telemetry/reset":
         rocket.reset()
     elif msg.topic == "/rocket/telemetry/notes":
-        rocket.setNotes(payload)
+        rocket.set_notes(payload)
+    elif msg.topic == "/rocket/camera/enable":
+        rocket.enable_camera(int(payload))
     elif msg.topic == "/rocket/telemetry/record":
         rocket.record(int(payload))
     elif msg.topic == "/rocket/parachute/deploy":
@@ -41,6 +47,7 @@ class Rocket(object):
     def __init__(self, simulate=None):     
         self.altitude = 0.0
         self.max_altitude = 0.0
+        self.camera_record = False
         self.recording = False
         self.record_reset = False
 
@@ -54,6 +61,7 @@ class Rocket(object):
         self.parachute_height = 15.0
         self.parachute_autodeploy = True
 
+        self.filename = None
         self.notes = None
         self.csvfile = None
 
@@ -63,6 +71,8 @@ class Rocket(object):
         self.client.subscribe("/rocket/telemetry/reset")
         self.client.subscribe("/rocket/telemetry/record")
         self.client.subscribe("/rocket/telemetry/notes")
+
+        self.client.subscribe("/rocket/camera/enable")
 
         self.client.subscribe("/rocket/parachute/deploy")
         self.client.subscribe("/rocket/parachute/auto")
@@ -77,46 +87,64 @@ class Rocket(object):
         self.pi = pigpio.pi()
         self.pi.set_servo_pulsewidth(SERVO_PIN, PARACHUTE_RESET)
 
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (1920, 1080) # full HD
+        self.camera.framerate = 30
+
         self.reset()
+
+        self.client.publish("/rocket/parachute/auto", "1")
+        self.client.publish("/rocket/camera/enable", "0")
+
         logging.info("Running flight computer")
 
-    def reset(self):
+    def reset_data(self):
         self.altitude = 0.0
         self.max_altitude = 0.0
         self.bmp280.sea_level_pressure = self.bmp280.pressure
+
+    def reset(self):
+        self.reset_data()
+
         self.client.publish("/rocket/telemetry/altitude", self.altitude)
         self.client.publish("/rocket/telemetry/altitude/max", self.max_altitude)
-
-        if self.simulate:
-            self.simulate.seek(0) # reset to start of simulation file
-            self.simulate.readline() # ignore headers
 
         logging.info("Reset Altitude")
 
         self.parachute_reset()
-        self.set_parachute_auto(True)
-        self.client.publish("/rocket/parachute/deploy", "0")
-        # self.client.publish("/rocket/parachute/auto", "1")
         self.client.publish("/rocket/telemetry/record", "0")
         logging.info("Reset Parachute")
 
-    def setNotes(self, notes):
+    def set_notes(self, notes):
         self.notes = notes
         logging.info(f"Set notes: {self.notes}")
+
+    def enable_camera(self, enable):
+        self.camera_record = enable
+        logging.info(f"Camera recording: {self.camera_record}")
 
     def record(self, state):
         self.recording = bool(state)
         if self.recording:
-            if self.csvfile:
-                self.csvfile.close()
-            today = datetime.now().strftime("%Y_%m_%d_%H_%M")
-            self.csvfile = open(f"/home/pi/{today}.csv", "w")
+            self.reset_data()
+            self.filename = datetime.now().strftime("%Y_%m_%d_%H_%M")
+            self.csvfile = open(f"{TELEMETRY_PATH}{self.filename}.csv", "w")
 
             if self.notes:
                 self.csvfile.write(f"{self.notes}\n")
 
             self.csvfile.write("time,altitude,deploy\n") # header
-            
+
+            if self.camera_record:
+                self.camera.start_recording(f"{TELEMETRY_PATH}{self.filename}.h264")        
+        else:
+            if self.csvfile:
+                self.csvfile.close()
+
+            if self.camera_record:
+                if self.camera.recording:
+                    self.camera.stop_recording()
+
         logging.info(f"Set recording to {self.recording}")
 
     def parachute_deploy(self):
@@ -150,30 +178,40 @@ class Rocket(object):
                 now = datetime.timestamp(datetime.now())
 
                 # Simulate altititude
+
                 if self.simulate:
                     line = self.simulate.readline()
-                    if line != "\n":
-                        self.altitude = float(line.split(",")[1])
+                    if line and line != "\n":
+                        simdata = line.split(",")
+                        if simdata:
+                            self.altitude = float(simdata[1])
+                            #self.parachute_deployed = float(simdata[2])
                         logging.info(f"Simulated")
                     else:
-                        self.recording = False # stop recording at end of file
+                        logging.info(f"End of simulation file")
                         self.simulate.seek(0) # reset to start of simulation file
                         self.simulate.readline() # ignore headers
 
-                logging.info(f"Reading altitude {self.altitude}")
                 self.csvfile.write(f"{now},{self.altitude},{int(self.parachute_deployed)}\n")
                 self.csvfile.flush()
-
+                
                 # auto parachute deploy
                 if self.parachute_autodeploy \
                 and not self.parachute_deployed \
                 and (self.max_altitude - self.altitude) > DROP_AFTER_APOGEE:
                     self.parachute_deploy()
+
+                    # take photo at apogee
+                    if self.camera_record:
+                        self.camera.capture(f"{TELEMETRY_PATH}{self.filename}.jpg", use_video_port=True)
+                        logging.info("Taking apogee photo")
+
                     logging.info(f"auto deploying parachute at height {self.altitude}")
 
             else:
-                delay = 1
+                delay = 1.0
 
+            logging.info(f"Reading altitude {self.altitude}")
             self.client.publish("/rocket/telemetry/altitude", self.altitude)
 
             if self.altitude > self.max_altitude:
